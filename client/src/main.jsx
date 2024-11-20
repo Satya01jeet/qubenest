32b467ba53849b6cddd6a7db5b6c4a0ad647d53f
Satya <sausatyajeet100@gmail.com>
2024-11-20 10:12:33 +0530

    horizontal overflow fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css' 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App className='overflow-x-hidden'/>
+    <div className='overflow-x-hidden w-full'>
+      <App />
+    </div>
   </StrictMode>,
 )
