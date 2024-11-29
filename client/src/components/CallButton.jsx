fcc200a57ee828f2794555bb674a40ebacf2d55b
Satya <sausatyajeet100@gmail.com>
2024-11-29 19:01:47 +0530

    changes number

--- client/src/components/CallButton.jsx
+++ client/src/components/CallButton.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const FloatingCallButton = () => {
   return (
     <a
-      href="tel:+919958895043"
+      href="tel:+917619636239"
       className="fixed inset-0 m-auto w-max h-max bg-gradient-to-r from-orange-400 to-orange-700 text-white rounded-lg px-4 py-3 flex items-center justify-center shadow-lg transition transform translate-y-0"
       aria-label="Call us at +919958895043"
       style={{ top: "90%", transform: "translateY(-50%)" }}
     >
-      📞 +91-9958895043
+      📞 +91-7619636239
     </a>
   );
 };
